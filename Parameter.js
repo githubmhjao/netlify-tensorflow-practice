d1c2732a7b7a8d3f4fa94dc141aab0ae6b74f9bd
githubmhjao <ntumhjao@gmail.com>
2021-01-23 00:44:12 +0800

    Create Parameter.js

---
+++ Parameter.js
@@ -0,0 +1,10 @@
+function Parameter() {
+  return (
+    <div className="container">
+      <div className="card-header">Parameter</div>
+      <div className="card-body" id="container-parameter" />
+    </div>
+  )
+}
+
+export default Parameter
