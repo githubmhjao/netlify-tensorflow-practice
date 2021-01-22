63a5aae4172480253a0ba8fb3ac1e08c4f51c8c4
githubmhjao <ntumhjao@gmail.com>
2021-01-23 00:41:42 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import Parameter from "./Parameter"
+import Scatter from "./Scatter"
+import Model from "./Model"
+import Train from "./Train"
+
+function App() {
+  return (
+    <>
+      <Parameter />
+      <Scatter />
+      <Model />
+      <Train />
+    </>
+  )
+}
+
+export default App
