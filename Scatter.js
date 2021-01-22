7adf94c4277c413198a304d0169f33b6a02f2ff0
githubmhjao <ntumhjao@gmail.com>
2021-01-23 00:45:17 +0800

    Create Scatter.js

---
+++ Scatter.js
@@ -0,0 +1,10 @@
+function Scatter() {
+  return (
+    <div className="container">
+      <div className="card-header">Scatter</div>
+      <div className="card-body" id="container-scatter" />
+    </div>
+  )
+}
+
+export default Scatter
